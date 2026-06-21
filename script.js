c28177a408b0b957994b8f2e51b984d55b0ae5c2
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-21 19:36:30 +0530

    solve iife

--- script.js
+++ script.js
@@ -15,16 +15,20 @@
 // HiName("Sung")
 
 
-function addUnlimited(...nums){
-    let sum = 0 
-    // nums.forEach(val=>{
-    //     sum += val
-    // })
-    let ans = nums.reduce(function(acc,val){
-        return  acc + val;
-    },0)
-    console.log(ans)
-    // console.log(sum)
-}
+// function addUnlimited(...nums){
+//     let sum = 0 
+//     // nums.forEach(val=>{
+//     //     sum += val
+//     // })
+//     let ans = nums.reduce(function(acc,val){
+//         return  acc + val;
+//     },0)
+//     console.log(ans)
+//     // console.log(sum)
+// }
+// addUnlimited(1,2,3,4,5,6,7,8,9,10)
 
-addUnlimited(1,2,3,4,5,6,7,8,9,10)
+
+(function(){
+    console.log("Run Instantly")
+})()
