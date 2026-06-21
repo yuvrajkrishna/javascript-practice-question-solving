3ee2ec55953c2f42d37c17a3f7f1ccdd4926744a
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-21 19:34:32 +0530

    used the reduce method to solve the previous question

--- script.js
+++ script.js
@@ -17,10 +17,14 @@
 
 function addUnlimited(...nums){
     let sum = 0 
-    nums.forEach(val=>{
-        sum += val
-    })
-    console.log(sum)
+    // nums.forEach(val=>{
+    //     sum += val
+    // })
+    let ans = nums.reduce(function(acc,val){
+        return  acc + val;
+    },0)
+    console.log(ans)
+    // console.log(sum)
 }
 
 addUnlimited(1,2,3,4,5,6,7,8,9,10)
