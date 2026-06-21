36e7c24b0255ab72b10aeb2ee65a979272143034
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-21 19:28:03 +0530

    made a function add Unlimited using rest parameter

--- script.js
+++ script.js
@@ -7,11 +7,20 @@
 // function add(a,b){
 //     return a+b
 // }
-
 // console.log(add(2,4))
 
-function HiName(name = "Guest"){
-    console.log(`Hi ${name}`)
+// function HiName(name = "Guest"){
+//     console.log(`Hi ${name}`)
+// }
+// HiName("Sung")
+
+
+function addUnlimited(...nums){
+    let sum = 0 
+    for(let i = 1 ; i <= nums.length; i++){
+        sum += i
+    }
+    console.log(sum)
 }
 
-HiName("Sung")
+addUnlimited(1,2,3,4,5,6,7,8,9,10)
